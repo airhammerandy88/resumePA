6991a1c2958e539c03adc5422da7c2f8d55b983b
airhammerandy88 <ajone355@wgu.edu>
2026-01-22 14:18:46 -0500

    root

--- jones_portfolio/datecon.js
+++
@@ -1,6 +0,0 @@
-
-const d = new Date();
-            document.getElementById("resnav").innerHTML = d; 
-
-let name = prompt("What shall I call you")
-console.log(`Welcome to my Office, ${name}.` );
---
+++ main/datecon.js
@@ -0,0 +1,6 @@
+
+const d = new Date();
+            document.getElementById("resnav").innerHTML = d; 
+
+let name = prompt("What shall I call you")
+console.log(`Welcome to my Office, ${name}.` );
